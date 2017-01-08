3ec70dd7e428b693d50189efde01104edbe6d1f6
Shannon Matloob <smtlb90@gmail.com>
2017-01-08 10:35:53 -0500

    Initial build csfController

--- controllers/coffeeShops.js
+++ controllers/coffeeShops.js
@@ -1,11 +1,12 @@
 //DEPENDENCIES
 var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
+var router = express.Router();
 var mongoose = require('mongoose');
+var Location = require('../models/location.js')
 
 //get '/' and display initial information (the only initial info wed have that is stored locally is the radius for the drop down if we do that.)
 
 //we will only need to have more routes if were displaying any data from our local database. Anything from our API will come from http requests viz
 
-//export router
+//EXPORT ROUTER
+module.exports = router;
