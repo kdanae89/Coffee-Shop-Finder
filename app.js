80cd763fd2e85208e33b3a0c7d5aeace7b39e95a
Kaylie Weable <kaylieweable@Kaylies-MacBook-Pro.local>
2017-01-07 21:40:59 -0800

    started pseudocode for app.js

---
+++ app.js
@@ -0,0 +1,26 @@
+//So i set up my pseudocode to give you guys a somewhat smooth easy to follow build.
+
+//declare angular
+
+//create our controller, calling this one findShops
+//name controller and http input for the function
+
+//declare "this" globally within the function so that all http requests can grab
+//set this equal to variable for use within the return of the http requests
+
+//the first thing i want to do is 'GET' 'coffee shop/cafe' data within a specific radius of our input zip code
+
+//this.getShops = function(zipcode)
+//$http({............
+// so: method = 'GET', url: 'url to our API
+
+
+
+
+//.then
+//     function(response)
+//        log response         (so for now this section will be guesswork until i can get into our API and find what data im getting back. how the response will be formatted.)
+
+
+
+//})
