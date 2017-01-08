9703b716136b03075c0141d80385eda0a2365792
Kaylie Weable <kaylieweable@Kaylies-MacBook-Pro.local>
2017-01-08 09:01:50 -0800

    worked on angular script link

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,10 @@
-//So i set up my pseudocode to give you guys a somewhat smooth easy to follow build.
-
 //DECLARE ANGULAR
 //added angular declaration
-var app = angular.module('cShopFinder', []);
+var app = angular.module('coffeeShopFinder', []);
 
 //create our controller, calling this one locatedShops
 app.controller('locatedShops', ['$http', function($http){
-  //moved our global this into our controller, its global to the controller
+  //moved our global this into our controller, its global to the controller, outside of the controller "this" is not the controller and thats what we need "this" to be.
   var controller = this;
   //turned our http request into a function so we could call it in our html if needed (say we need it in a click)
   this.getShops = function(zipcode){
