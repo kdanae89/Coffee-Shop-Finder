72d55fdd1bd0b5539b222029517f350204119886
Kaylie Weable <kaylieweable@Kaylies-MacBook-Pro.local>
2017-01-09 09:09:48 -0800

    deleted old controller

--- public/js/app.js
+++ public/js/app.js
@@ -2,53 +2,6 @@
 //added angular declaration
 var app = angular.module('coffeeShopFinder', []);
 
-//create our controller, calling this one locatedShops
-app.controller('locatedShops', ['$http', function($http){
-  //moved our global this into our controller, its global to the controller, outside of the controller "this" is not the controller and thats what we need "this" to be.
-  var controller = this;
-  //turned our http request into a function so we could call it in our html if needed (say we need it in a click)
-  this.getShops = function(zipcode){
-  $http({
-    method:'GET',
-    // url:''
-  }).then(
-      function(response) {
-        //log to test
-        console.log(response);
-        controller.shops = response.data;
-        //(so for now this section will be guesswork until i can get into our API and find what data im getting back, and how the response will be formatted.)
-      },
-      function(response) {
-        console.log(response);
-      });
-    }
-
-}]);
-
-//create our controller, calling this one locatedShops
-app.controller('locatedShops', ['$http', function($http){
-  //moved our global this into our controller, its global to the controller, outside of the controller "this" is not the controller and thats what we need "this" to be.
-  var controller = this;
-  //turned our http request into a function so we could call it in our html if needed (say we need it in a click)
-  this.getShops = function(zipcode){
-  $http({
-    method:'GET',
-    // url:
-
-  }).then(
-      function(response) {
-        //log to test
-        console.log(response);
-        controller.shops = response.data;
-        //(so for now this section will be guesswork until i can get into our API and find what data im getting back, and how the response will be formatted.)
-      },
-      function(response) {
-        console.log(response);
-      });
-    }
-
-}]);
-
 
 
 //connect maps
@@ -70,7 +23,7 @@ var initMap = function() {
           radius:8047,
           types: ['cafe']
         };
-      
+
         var service = new google.maps.places.PlacesService(map);
 
         service.nearbySearch(request, callback);
