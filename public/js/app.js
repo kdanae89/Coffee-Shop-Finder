4924555b29e644c5d5ec219250c701555dff05ad
Kaylie Weable <kaylieweable@Kaylies-MacBook-Pro.local>
2017-01-09 15:02:18 -0800

    didnt edit anything

--- public/js/app.js
+++ public/js/app.js
@@ -1,31 +1,6 @@
 //DECLARE ANGULAR
 //added angular declaration
 var app = angular.module('coffeeShopFinder', []);
-var geocoder;
-var map;
-
-//create our controller, calling this one locatedShops
-app.controller('locatedShops', ['$http', function($http){
-  //moved our global this into our controller, its global to the controller, outside of the controller "this" is not the controller and thats what we need "this" to be.
-  var controller = this;
-  //turned our http request into a function so we could call it in our html if needed (say we need it in a click)
-  this.getShops = function(zipcode){
-  $http({
-    method:'GET',
-    url:''
-  }).then(
-      function(response) {
-        //log to test
-        console.log(response);
-        controller.shops = response.data;
-        //(so for now this section will be guesswork until i can get into our API and find what data im getting back, and how the response will be formatted.)
-      },
-      function(response) {
-        console.log(response);
-      });
-    }
-
-}]);
 
 //connect maps
 var initMap = function() {
@@ -61,14 +36,8 @@ var initMap = function() {
   var mapcenter = {lat: 37, lng: -95};
   map = new google.maps.Map(document.getElementById('map'), {
     zoom: 14,
-    center: mapcenter;
-  });
-  new google.maps.Map(document.getElementById('map'), {
-  zoom: 4,
-  center: mapcenter;
-  });
-
-
+    center: mapcenter
+  })
 
   var request = {
     location: center,
@@ -77,9 +46,12 @@ var initMap = function() {
     radius:8047,
     type: ['cafe']
   };
+
   var service = new google.maps.places.PlacesService(map);
   service.nearbySearch(request, callback);
+  console.log(request);
 }
+
   function callback(results, status) {
     if(status == google.maps.places.PlacesServicesStatus.OK){
       for (var i =0; i < results.length; i++){
@@ -88,6 +60,16 @@ var initMap = function() {
     }
   }
 
+  // var service = new google.maps.places.PlacesService(map);
+  // service.nearbySearch({
+  //   location: pyrmont,
+  //   radius: 500,
+  //   type: ['cafe']
+  // }, function(results){
+  //   console.log(results)
+  // });
+  //
+  // console.log('hi');
   function createMarker(place){
     var placeLoc = place.geometry.location;
     var marker = new google.maps.Marker ({
@@ -104,4 +86,3 @@ var initMap = function() {
 //})
 
   google.maps.event.addDomListener(window, 'load', initialize)
-
