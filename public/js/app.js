cc451880ed4ec09d243a53fe2ec5763b8a3ff7c3
Andy Sartori <Andy@AndyMBP.local>
2017-01-09 07:18:55 -0800

    map api

--- public/js/app.js
+++ public/js/app.js
@@ -38,8 +38,35 @@ var initMap = function() {
         zoom: 4,
         center: uluru
         });
+
+        var request = {
+          location: center,
+          radius:8047,
+          types ['cafe']
+        };
+
+        var service = new google.maps.places.PlacesService(map);
+
+        service.nearbySearch(request, callback);
       }
 
+      function callback(results, status) {
+        if(status == google.maps.places.PlacesServicesStatus.OK){
+          for (var i =0; i < results.length; i++){
+            createMarker(results[i]);
+          }
+        }
+      }
+
+      function createMarker(place){
+        var placeLoc = place.geometry.location;
+        var marker = new google.maps.Marker ({
+          map: map,
+          position: place.geometry.location
+        });
+      }
+
+      google.maps.event.addDomListener(window, 'loal', initialize)
 
 
 
