3a75c5c1cb2f19bec4a56259fcc251416330c2e6
Kaylie Weable <kaylieweable@Kaylies-MacBook-Pro.local>
2017-01-10 11:38:31 -0800

    fixing app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,79 +1,70 @@
 //DECLARE ANGULAR
-//added angular declaration
-var app = angular.module('coffeeShopFinder', []);
+  var app = angular.module('coffeeShopFinder', []);
 
-<<<<<<< HEAD
+ var geocoder;
+ var map;
 
-=======
-//create our controller, calling this one locatedShops
->>>>>>> e2f81b6d0139c5b408dc3c368c9c5f04a81ce0ee
+  //create our controller, calling this one locatedShops
 
-//create our controller, calling this one locatedShops
-app.controller('locatedShops', ['$http', function($http){
-  //moved our global this into our controller, its global to the controller, outside of the controller "this" is not the controller and thats what we need "this" to be.
-  var controller = this;
-  //turned our http request into a function so we could call it in our html if needed (say we need it in a click)
-  this.getShops = function(zipcode){
-  $http({
-    method:'GET',
-    url:''
-  }).then(
-      function(response) {
-        //log to test
-        console.log(response);
-        controller.shops = response.data;
-        //(so for now this section will be guesswork until i can get into our API and find what data im getting back, and how the response will be formatted.)
-      },
-      function(response) {
-        console.log(response);
-      });
-    }
+  //connect maps
+  var initMap = function() {
 
-}]);
-
-//connect maps
-var initMap = function() {
-        var uluru = {lat: 37, lng: -95};
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 12,
-          center: uluru
-        });
-        new google.maps.Map(document.getElementById('map'), {
-        zoom: 4,
-        center: uluru
-        });
-
-        var request = {
-          location: center,
-          radius:8047,
-          types ['cafe']
-        };
-
-        var service = new google.maps.places.PlacesService(map);
-
-        service.nearbySearch(request, callback);
-      }
-
-      function callback(results, status) {
-        if(status == google.maps.places.PlacesServicesStatus.OK){
-          for (var i =0; i < results.length; i++){
-            createMarker(results[i]);
+          var uluru = {lat: 37, lng: -95};
+          var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 12,
+            var initMap = function() {
+              createMarker(results[i]);
+            }
           }
+       }
+
+       function createMarker(place){
+         var placeLoc = place.geometry.location;
+         var marker = new google.maps.Marker ({
+           map: map,
+           position: place.geometry.location
+         });
+   var mapcenter = {lat: 37, lng: -95};
+   map = new google.maps.Map(document.getElementById('map'), {
+     zoom: 14,
+     center: mapcenter;
+   });
+   new google.maps.Map(document.getElementById('map'), {
+   zoom: 4,
+   center: mapcenter;
+   });
+
+
+
+   var request = {
+     location: center,
+     // (In meters, estimates roughly 5 miles)
+     postalCode: 'postalCode',
+     radius:8047,
+     type: ['cafe']
+   };
+   var service = new google.maps.places.PlacesService(map);
+   service.nearbySearch(request, callback);
+ }
+   function callback(results, status) {
+     if(status == google.maps.places.PlacesServicesStatus.OK){
+       for (var i =0; i < results.length; i++){
+         createMarker(results[i]);
         }
-      }
+     }
+   }
 
-      function createMarker(place){
-        var placeLoc = place.geometry.location;
-        var marker = new google.maps.Marker ({
-          map: map,
-          position: place.geometry.location
-        });
-      }
-
-      google.maps.event.addDomListener(window, 'loal', initialize)
-
+   function createMarker(place){
+     var placeLoc = place.geometry.location;
+     var marker = new google.maps.Marker ({
+       map: map,
+       position: place.geometry.location
+     });
+   }
 
 
 
+   google.maps.event.addDomListener(window, 'load', initialize)
 
-//})
+
+  //})
