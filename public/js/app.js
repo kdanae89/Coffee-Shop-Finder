b43a4732724051f190b3b1d9378c05e3b8a491de
Shannon Matloob <smtlb90@gmail.com>
2017-01-12 14:37:04 -0500

    Fix DOM error by adding $apply

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,7 @@
 var app = angular.module('coffeeShopFinder', []);
 
 //create our controller, calling this one locatedShops
-app.controller('locatedShops', ['$http', function($http){
+app.controller('locatedShops', ['$http', '$scope', function($http, $scope){
   //global this
   var controller = this;
   //function to get coffee shops
@@ -13,62 +13,69 @@ app.controller('locatedShops', ['$http', function($http){
       url:'https://maps.googleapis.com/maps/api/geocode/json?components=postal_code:'+this.searchQuery+'&key=AIzaSyAZh1fM4eOg-ovT68WXnfIDgSYu4FU5HYM',
     })
     .then(function(response) {
-      var lat = response.data.results[0].geometry.location.lat;
-      var lng = response.data.results[0].geometry.location.lng;
-      //  console.log(lat, lng);
-      var LatLng = lat.toString().concat(',',lng);
+        var lat = response.data.results[0].geometry.location.lat;
+        var lng = response.data.results[0].geometry.location.lng;
+        //  console.log(lat, lng);
+        var LatLng = lat.toString().concat(',',lng);
 
-      var map;
-      var service;
+        var map;
+        var service;
 
-      function initialize() {
-        var locatedAt = new google.maps.LatLng(lat,lng);
-        // console.log(locatedAt);
+        function initialize() {
+          var locatedAt = new google.maps.LatLng(lat,lng);
+          // console.log(locatedAt);
 
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: locatedAt,
-          zoom: 15
-        });
+          map = new google.maps.Map(document.getElementById('map'), {
+            center: locatedAt,
+            zoom: 15
+          });
 
-        var request = {
-          location: locatedAt,
-          types: ['cafe'],
-          rankBy: google.maps.places.RankBy.DISTANCE
-        };
+          var request = {
+            location: locatedAt,
+            types: ['cafe'],
+            rankBy: google.maps.places.RankBy.DISTANCE
+          };
 
-        service = new google.maps.places.PlacesService(map);
-        service.nearbySearch(request, callback);
+          service = new google.maps.places.PlacesService(map);
+          // service.nearbySearch(request, callback);
+          service.nearbySearch(request, function(results) {
+            $scope.$apply(function() {
+              console.log(results);
+              controller.places = results;
+            })
+          });
 
-        var infowindow = new google.maps.InfoWindow({
-          content: "hi kaylie"
-        });
 
-        var marker = new google.maps.Marker({
-          position: locatedAt,
-          map: map,
-          title: 'Details'
-        });
-        marker.addListener('click', function() {
-          infowindow.open(map, marker);
-        });
+          var infowindow = new google.maps.InfoWindow({
+            content: "hi kaylie"
+          });
+
+          var marker = new google.maps.Marker({
+            position: locatedAt,
+            map: map,
+            title: 'Details'
+          });
+          marker.addListener('click', function() {
+            infowindow.open(map, marker);
+          });
       }
-
       initialize();
+  })
 
-    })
+
 
     function callback(results, status) {
       if (status == google.maps.places.PlacesServiceStatus.OK) {
         var shops = [];
         results.forEach(function(shop) {
           shops.push(shop);
-      });
-      controller.places = shops;
-      console.log(controller.places);
+        });
+        controller.places = shops;
+        console.log(controller.places);
       // return controller.places;
-        }
       }
     }
+  }
 
 }]);
 
