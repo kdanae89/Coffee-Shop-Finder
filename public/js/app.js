bcdcc9625ddc79214dcffbad1d0deef1326c4f8e
Kaylie Weable <kaylieweable@Kaylies-MacBook-Pro.local>
2017-01-12 08:14:59 -0800

    deleted unneeded logs

--- public/js/app.js
+++ public/js/app.js
@@ -64,9 +64,7 @@ app.controller('locatedShops', ['$http', function($http){
           shops.push(shop);
       });
       controller.places = shops;
-      console.log(shops.name, ": ", shops.vicinity);
       console.log(controller.places);
-      console.log(controller.places.name);
       // return controller.places;
         }
       }
