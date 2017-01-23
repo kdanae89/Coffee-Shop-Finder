4e459375d028bf3eeb5559c69516242dbd362443
Shannon Matloob <smtlb90@gmail.com>
2017-01-23 08:55:51 -0500

    Add Marker and Info Window; Fix most bugs

--- public/js/app.js
+++ public/js/app.js
@@ -10,90 +10,77 @@ app.controller('locatedShops', ['$http', '$scope', function($http, $scope){
     //grab coffee shops by zip
     $http({
       method: 'GET',
-      url:'https://maps.googleapis.com/maps/api/geocode/json?components=postal_code:'+this.searchQuery+'&key=process.env.API_KEY',
+      url:'https://maps.googleapis.com/maps/api/geocode/json?components=postal_code:'+this.searchQuery+'&key=AIzaSyDtX8DgraL4-rsLJOk4QEYYa8zUUQR6uUE',
     })
+    // get latitude and longitude from response
     .then(function(response) {
-        var lat = response.data.results[0].geometry.location.lat;
-        var lng = response.data.results[0].geometry.location.lng;
-        //  console.log(lat, lng);
-        var LatLng = lat.toString().concat(',',lng);
+      var lat = response.data.results[0].geometry.location.lat;
+      var lng = response.data.results[0].geometry.location.lng;
+      console.log(lat, lng);
+
+      function initialize() {
+        var locatedAt = new google.maps.LatLng(lat,lng);
+        console.log(locatedAt);
 
         var map;
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: locatedAt,
+          zoom: 15
+        });
+
+        var request = {
+          location: locatedAt,
+          types: ['cafe'],
+          rankBy: google.maps.places.RankBy.DISTANCE
+        };
+
+        // initalize service API variable
         var service;
-        var geocoder = new google.maps.Geocoder;
+        service = new google.maps.places.PlacesService(map);
+        service.nearbySearch(request, getPlaces);
+      }
 
-        function initialize() {
-          var locatedAt = new google.maps.LatLng(lat,lng);
-          // console.log(locatedAt);
-
-          map = new google.maps.Map(document.getElementById('map'), {
-            center: locatedAt,
-            zoom: 15
-          });
-
-          var request = {
-            location: locatedAt,
-            types: ['cafe'],
-            rankBy: google.maps.places.RankBy.DISTANCE
-          };
-
-
-          service = new google.maps.places.PlacesService(map);
-          $scope.$apply(function() {
-            service.nearbySearch(request, getPlaces);
-            service.nearbySearch(request, getMarkers);
-          })
-        }
-        initialize();
-      })
       function getPlaces(results, status) {
         if (status == google.maps.places.PlacesServiceStatus.OK) {
           var shops = [];
           results.forEach(function(shop) {
             shops.push(shop);
+            var lat = shop.geometry.location.lat();
+            var lng = shop.geometry.location.lng();
+            console.log(lat, lng);
+            var LatLng = new google.maps.LatLng(lat,lng);
+            var marker = new google.maps.Marker({
+              position: LatLng
+            });
+
+            var contentString = '<ul id="results">' +
+              '<li>  {{shop.name}} @ {{shop.vicinity}}</li>' +
+            '</ul>';
+
+            var infowindow = new google.maps.InfoWindow({
+              content: contentString
+            });
+
+            marker.addListener('click', function() {
+              infowindow.open(map, marker);
+            });
+
+            console.log(shop);
           });
           controller.places = shops;
           console.log(controller.places);
         }
       }
-
-      function getLatLng() {
-        var LatLng;
-        geocoder.geocode( { 'LatLng' : LatLng }, function(results) {
-          LatLng = results.geometry.location
-        });
-        return LatLng;
-      }
-
-      var newMarker = new google.maps.Marker({
-        position: LatLng
-      });
-
-      function getMarkers(results, status) {
-        if (status == google.maps.places.PlacesServiceStatus.OK) {
-          results.forEach(function(result) {
-            getLatLng();
-            var infowindow = new google.maps.InfoWindow({
-              content: {
-                Name: result.name,
-                Rating: result.rating,
-                Address: result.vicinity,
-              }
-            });
-            newMarker.addListener('click', function() {
-              infowindow.open(map, newMarker);
-            });
-          })
-        }
-      }
+      $scope.$apply(initialize());
+    })
   }
-
 }]);
 
-//connect maps
-var initMap = function() {
+// connect maps
+function initMap() {
+  var map;
   var location = {lat: 40.09024, lng: -100.712891};
-  new google.maps.Map(document.getElementById('map'), {
+  map = new google.maps.Map(document.getElementById('map'), {
     zoom: 4,
     center: location
   });
