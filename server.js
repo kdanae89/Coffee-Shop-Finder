7d9e80216107dde53c40ecb02cd78365faa7986a
Kaylie Weable <kaylieweable@Kaylies-MacBook-Pro.local>
2017-04-13 11:33:43 -0700

    working map with key

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var apiKey = require('dotenv').config();
 
 // PORT
 var port = process.env.PORT || 3000;
